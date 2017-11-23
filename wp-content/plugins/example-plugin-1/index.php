cdbcb415f72f58304379c97e70bcf46f9da084fb
Rostyslav Naryzhniak <ros10@meta.ua>
2017-11-23 12:13:33 +0000

    just connected js and css

--- wp-content/plugins/example-plugin-1/index.php
+++ wp-content/plugins/example-plugin-1/index.php
@@ -24,3 +24,36 @@ function option1()
 {
 	echo "its first option for example";
 }
+
+
+
+add_action( 'woocommerce_before_header', 'art_wp_before_header');
+
+	function art_wp_before_header (){
+		echo 'Мой первый хук: woocommerce_before_header';
+	}
+	
+	
+$page_title='My New Settings Page';
+$menu_title='New Settings';
+$capability='manage_options';
+$menu_slug='site-options';
+$function='add_my_setting';
+$icon_url='';
+$position=4;
+
+add_action( 'new_admin_menu', 'register_my_custom_menu_page' );
+
+function register_my_custom_menu_page(){
+	add_menu_page( $page_title, $menu_title, $capability, $menu_slug, 'myplugin/myplugin-admin.php', '', plugins_url( 'myplugin/images/icon.png' ), 6 );
+}
+
+function connected_scripts() {
+	wp_enqueue_style( 'connected-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'my-style', plugins_url('example-plugin-1/css') . '/my-style.css' );
+	if (is_single()){
+		wp_enqueue_script( 'my-script', plugins_url('example-plugin-1/js') . '/ember-cli-build.js', array('jquery'), false, true );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'connected_scripts' );
